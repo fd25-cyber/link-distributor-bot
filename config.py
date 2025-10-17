3cc158d49cb9377136a05717a897303eedafed6e
fd25-cyber <fdookomorka@gmail.com>
2025-10-17 10:57:57 +0300

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "7697314753:AAH6K5aA0UDY_hleof-PgE_rWJtAp1oJK-o"
-ADMIN_IDS = [7054164521]  # Telegram ID админа
+BOT_TOKEN = ""
+ADMIN_IDS = []  # Telegram ID админа
 DATA_FILE = "data.json"
